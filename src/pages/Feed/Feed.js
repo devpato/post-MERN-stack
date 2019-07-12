b08b4cd5b507a6447e6ee746f21a7430be32d4fa
Patricio <wearetamo@gmail.com>
2019-07-11 21:14:59 -0400

    token header

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page)
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -124,6 +128,9 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      },
       body: formData
     })
       .then(res => {
@@ -177,7 +184,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
